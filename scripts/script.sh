4f292601e5e2943a118a0e2d71cdc03513e29474
houssemzaier <houssem.zaier@gmail.com>
2020-01-26 00:56:46 +0100

    waza

--- scripts/script.sh
+++ scripts/script.sh
@@ -7,5 +7,16 @@
 var="Hello World"
 # print it
 echo "$var"
+echo "$NonSecret"
 # Another way of printing it
-printf "%s\n" "$var"
+#printf "%s\n" "$var"
+#KEYSTORE_PATH=".keystore/boilerplate_keystore.jks"
+#ALIAS="keystore"
+#KEY_PASSWORD="H888h888"
+#KEYSTORE_PASSWORD="H888h888"
+#STORE_FILE_BASE64="aaaaaaaaaa"
+##echo $STORE_FILE_BASE64 | base64 -d > my.keystore
+#echo "storeFile=$KEYSTORE_PATH" > signing.properties
+#echo "storePassword=$KEYSTORE_PASSWORD" >> signing.properties
+#echo "keyAlias=$ALIAS" >> signing.properties
+#echo "keyPassword=$KEY_PASSWORD" >> signing.properties
